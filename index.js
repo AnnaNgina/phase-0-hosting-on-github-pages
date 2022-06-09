9223f56d03c1e3ab35eab87f8d237a8f9be842de
annastacia <annastaciangina96@gmail.com>
2022-06-09 22:45:46 +0300

    commit tree 0830d0fb2a5ee82a84e5743659b39c9b8bf411b8

---
+++ index.js
@@ -0,0 +1,30 @@
+function breed (){
+    const name="brayo";
+    const height ="five";
+    console.log(`the name ${name} and ${height}`);
+return breed;
+}
+function exerciseByronThePoodle() {
+    const dogName = "Byron";
+    const dogBreed = "poodle";
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    console.log(`Walk to the park ${dogName} the ${dogBreed}`);
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+   return exerciseByronThePoodle();
+   
+}
+//function razzle(){
+    //console.log('hello its me');
+//}
+//razzle();
+function razzle(lawyer ='bill', son='jose'){
+
+    console.log(`her name ${laywer} and ${'son'}`)
+
+    
+}
+razzle();
+razzle('methu', 'ann')
